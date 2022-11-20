8c62ef6ce72013833e971471b143e43b069da9f1
kaan~ <81658277+whyGhosT@users.noreply.github.com>
2022-11-20 20:04:44 +0300

    rainbow fix

--- cl_dll/hud/rainbow.cpp
+++ cl_dll/hud/rainbow.cpp
@@ -50,6 +50,8 @@ void CRainbow::GetRainbowColor(int x, int y, int &r, int &g, int &b)
     phase += m_pCvarRainbowXPhase->value * x;
     phase += m_pCvarRainbowYPhase->value * y;
     phase = fmod(phase, 360);
+	if ( phase < 0 )
+		phase += 360;
 
     HSVtoRGB(phase, m_flSat, m_flVal, r, g, b);
 }
@@ -96,7 +98,14 @@ void CRainbow::SPR_DrawAdditiveRainbow(int frame, int x, int y, const rect_s *pr
 
 int CRainbow::DrawString(int x, int y, const char *str, int r, int g, int b)
 {
-    return DrawRainbowString(x, y, str, gHUD.m_Rainbow.m_pfnDrawString);
+	if ( r == 0 && g == 0 && b == 0 )
+	{
+		return gHUD.m_Rainbow.m_pfnDrawString( x, y, str, r, g, b );
+	}
+	else
+	{
+		return DrawRainbowString( x, y, str, gHUD.m_Rainbow.m_pfnDrawString );
+	}
 }
 
 int CRainbow::DrawStringReverse(int x, int y, const char *str, int r, int g, int b)
